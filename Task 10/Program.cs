2223bba2d8be193ae5ac5e43443a60ae922a29fc
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-04-02 21:15:14 +0500

    Task 10 added

---
+++ Task 10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number>=100 && number < 1000)
+{
+  int Mid = (number%100 - number%10)/10;
+  Console.WriteLine($"Вторая цифра числа {number} является {Mid}");
+}
+else 
+Console.WriteLine("Это число не трехзначное");
+
+
